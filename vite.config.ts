f766a470aac100544418caae2ef1a32033eb1f04
Tanmay Tekale <75844090+tanmaytekale@users.noreply.github.com>
2025-04-20 13:06:11 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/polycosmos-website/',
   server: {
-    host: "::",
+    host: '::',
     port: 8080,
   },
   plugins: [
