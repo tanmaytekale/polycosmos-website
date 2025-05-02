e4994f8280ac0768127d591e0430b0f747cafcf4
Tanmay Tekale <75844090+tanmaytekale@users.noreply.github.com>
2025-05-03 03:30:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   resolve: {
-    alias: {"@": path.resolve(__dirname, "./src")}
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    },
   },
-  build: {
-    outDir: './', // this will build files in root
-    emptyOutDir: false, // so it doesn't delete your git files
-  }
 }));
