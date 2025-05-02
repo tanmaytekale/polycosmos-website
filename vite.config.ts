ba4e07390186593d214508ca111a61e841a23096
Tanmay Tekale <75844090+tanmaytekale@users.noreply.github.com>
2025-05-03 03:30:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/your-repo-name/', // <-- replace with your GitHub repo name (case-sensitive)
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  base: './',
+  plugins: [
+    react()
+  ],
   resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") }
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    },
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
-});
+}));
